9e2bab2a197faedf3d191a9a93e0d5720e724fae
白宇东 <summer88123@163.com>
2023-10-30 19:53:09 +0800

    feat: 修改添加群成员失败提示
    --story=1009038 --user=白宇东 【移动端】移动端体验优化2312
    https://www.tapd.cn/23787791/s/1792501

--- plugin/setting/GeneralUpdate.ts
+++ plugin/setting/GeneralUpdate.ts
@@ -1,29 +1,26 @@
-import {InteractionManager} from 'react-native';
 import Toast from 'react-native-root-toast';
 import i18n from 'i18n-js';
-import {Delegate, PageKeys, Typings} from '../../standard';
+import { Delegate, PageKeys, Typings } from '../../standard';
 
 export async function onAddMembers(props: Typings.Action.Setting.Params,
                                    memberUserIds: string[]): Promise<void> {
-    const {imId, chatType, onDataChange, navigation} = props;
+    const { imId, chatType, onDataChange, navigation } = props;
     const isGroup = chatType === Typings.Conversation.ChatType.Group;
     if (isGroup) {
         try {
             await Delegate.model.Group.addMembers(imId, memberUserIds);
             onDataChange();
         } catch (err) {
-            Toast.show(i18n.t('IMToastError', {
-                action: i18n.t('IMSettingAddGroupMember'),
-            }));
+            Toast.show(i18n.t('IMSettingAddGroupMemberFail'));
         }
     } else {
         const newMembers = [imId, ...memberUserIds];
         try {
             const result = await Delegate.model.Conversation.createOne(newMembers);
             navigation.navigate(PageKeys.ChatDetail, {
-                    imId: result.imId,
-                    chatType: result.chatType,
-                });
+                imId: result.imId,
+                chatType: result.chatType,
+            });
         } catch (err) {
             Toast.show(i18n.t('IMToastError', {
                 action: i18n.t('IMCreateGroup'),
@@ -35,7 +32,7 @@ export async function onAddMembers(props: Typings.Action.Setting.Params,
 
 export async function onRemoveMembers(props: Typings.Action.Setting.Params,
                                       memberUserIds: string[]): Promise<void> {
-    const {imId, onDataChange} = props;
+    const { imId, onDataChange } = props;
     try {
         await Delegate.model.Group.removeMembers(imId, memberUserIds);
         onDataChange();
