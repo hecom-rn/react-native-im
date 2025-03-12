97ea4d61b49f6b31ee63a16c25d546e790fc44d8
白宇东 <summer88123@163.com>
2025-03-12 14:42:06 +0800

    feat: 更换消息时间展示方式

--- standard/util/DateUtil.ts
+++ standard/util/DateUtil.ts
@@ -1,6 +1,9 @@
-import * as RNLocalize from 'react-native-localize';
-import i18n from 'i18n-js';
 import {General} from "standard/typings/Message";
+
+function getDayOfWeek(date) {
+    const days = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+    return days[date.getDay()];
+}
 
 export function showDateTime(
     timestamp: number,
@@ -11,22 +14,16 @@ export function showDateTime(
     }
     const now = new Date();
     const that = new Date(timestamp);
-    const locale = i18n.locale;
-    const options: Intl.DateTimeFormatOptions = {};
     const isSameDay = sameDay(now, that);
     const isSameWeek = sameWeek(now, that);
-    const timeStr = (isSameDay || showTime) ? that.toLocaleTimeString(locale, {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: RNLocalize.uses24HourClock(),
-    }) : '';
-    const dayStr = isSameDay ? '' : isSameWeek ? that.toLocaleDateString(locale, {
-        weekday: 'long',
-    }) : that.toLocaleDateString(locale, {
-        year: 'numeric',
-        month: showTime ? 'short' : 'numeric',
-        day: 'numeric',
-    });
+    const year = that.getFullYear();
+    const month = String(that.getMonth() + 1); // 月份从0开始，需要加1
+    const day = String(that.getDate());
+    const hours = String(that.getHours()).padStart(2, '0');
+    const minutes = String(that.getMinutes()).padStart(2, '0');
+    const dayOfWeek = getDayOfWeek(that);
+    const timeStr = (isSameDay || showTime) ? `${hours}:${minutes}` : '';
+    const dayStr = isSameDay ? '' : isSameWeek ? dayOfWeek : `${year}-${month}-${day}`;
     return [dayStr, timeStr].filter(i => i.length > 0).join(' ');
 }
 
