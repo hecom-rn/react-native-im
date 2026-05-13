449fe78b423a149b5a2d64a5c0e5958dfbd5486a
Ma Shuai <mashuai_hy@163.com>
2026-05-13 17:14:29 +0800

    fix: 更新时间格式化

--- standard/util/DateUtil.ts
+++ standard/util/DateUtil.ts
@@ -1,5 +1,5 @@
-import {General} from "standard/typings/Message";
 import { TimeUtils } from '@hecom/aDate';
+import { General } from "standard/typings/Message";
 
 export function showDateTime(
     timestamp: number,
@@ -9,15 +9,7 @@ export function showDateTime(
         return '';
     }
     const date = TimeUtils.create(timestamp);
-
-    // 获取各个时间部分
-    const year = date.getYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份是0-11
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHour()).padStart(2, '0');
-    const minutes = String(date.getMinute()).padStart(2, '0');
-
-    return showTime ? `${year}-${month}-${day} ${hours}:${minutes}` : `${year}-${month}-${day}`;
+    return showTime ? date.format('YYYY-MM-DD HH:mm') : date.format('YYYY-MM-DD');
 }
 
 export function needShowTime(forward: General, current: General) {
